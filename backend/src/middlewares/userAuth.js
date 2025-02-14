f448e648176336b50fd95b9981b4cae1342c0974
Nandanakrishna03 <nandanakrishna75@gmail.com>
2025-02-14 21:11:49 +0530

    userAuth.js

--- backend/src/middlewares/userAuth.js
+++ backend/src/middlewares/userAuth.js
@@ -1,18 +1,22 @@
 import jwt from "jsonwebtoken";
-export const userAuth=(req,res,next)=>{
+
+export const userAuth = (req, res, next) => {
     try {
-        const token=req.cookies.token;
+        const token = req.cookies.token;
+
         if (!token) {
-            return res.status(401).json({message:"user not authorised",success:false})
-        }
-        const tokenVerified=jwt.verify(token, process.env.JWT_SECRET_KEY);
-        if (!tokenVerified) {
-            return res.status(401).json({message:"user not authorised",success:false})
+            return res.status(401).json({ message: "User not authorized", success: false });
         }
 
-        req.user=tokenVerified;
+        const tokenVerified = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+        if (!tokenVerified) {
+            return res.status(401).json({ message: "User not authorized", success: false });
+        }
+
+        req.user = tokenVerified;
         next();
     } catch (error) {
-        return res.status(401).json({message:error.message||"user autherization failed",success:false})
+        return res.status(401).json({ message: error.message || "User authorization failed", success: false });
     }
-}
+};
