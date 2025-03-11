9ab4e85d56a33dd8b9aaa7d899fb85cc54410c18
NandanaKrishna03 <nandanakrishna75@gmail.com>
2025-03-11 16:35:26 +0530

    Update adminRoutes.js

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -8,7 +8,9 @@ import {
     getAllBookings, 
     adminLogout, 
     deleteUser,
-    deleteManager
+    deleteManager,
+    deleteTurf,
+    deleteBooking
 } from "../controllers/adminController.js";
 import { adminAuth } from "../middlewares/adminAuth.js";
 import { getAllManagers } from "../controllers/managerController.js";
@@ -26,7 +28,9 @@ router.get("/getAllManagers", adminAuth, getAllManagers);
 router.get("/user", adminAuth, getAllUsers);
 router.get("/turfs", adminAuth, getAllTurfs);
 router.get("/bookings", adminAuth, getAllBookings);
+
 router.delete("/user/:userId", adminAuth, deleteUser); 
 router.delete("/manager/:managerId", adminAuth, deleteManager);
-
+router.delete("/turf/:turfId", adminAuth, deleteTurf);
+router.delete("/booking/:bookingId", adminAuth, deleteBooking);
 export { router as AdminRouter };
